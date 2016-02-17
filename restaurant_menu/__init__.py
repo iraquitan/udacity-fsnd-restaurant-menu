bef478c29150ecc98d5ce4d53994e595da13c428
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2016-02-17 17:58:55 -0300

    feat: Add basic logging to project

--- restaurant_menu/__init__.py
+++ restaurant_menu/__init__.py
@@ -7,6 +7,8 @@
  * Date: 2/17/16
  * Time: 12:02 AM
 """
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from flask.ext.mail import Mail
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -16,4 +18,9 @@ app.config.from_object('config')
 app.config.from_pyfile('config.py')
 mail = Mail(app)
 db = SQLAlchemy(app)
+if app.debug:
+    log_file_handler = RotatingFileHandler(
+        '../restaurant-menu/restaurant_menu/basic-logging.txt',
+        maxBytes=10000, backupCount=0)
+    app.logger.addHandler(log_file_handler)
 from restaurant_menu.views import restaurant, menu_item
