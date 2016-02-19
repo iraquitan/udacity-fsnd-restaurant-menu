90a2a01cc487142e5b9f8bce7643a6486c822c18
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2016-02-19 18:19:04 -0300

    feat: Add User model

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -10,10 +10,12 @@
 from restaurant_menu import db
 
 
-class Restaurant(db.Model):
-    __tablename__ = 'restaurant'
+class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False)
+    picture = db.Column(db.String(250))
 
     @property
     def serialize(self):
@@ -22,19 +24,42 @@ class Restaurant(db.Model):
         """
         return {
             'id': self.id,
-            'name': self.name
+            'name': self.name,
+            'email': self.email,
+            'picture': self.picture
+        }
+
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurant'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
+
+    @property
+    def serialize(self):
+        """
+        Returns object data in easily serializable format
+        """
+        return {
+            'id': self.id,
+            'name': self.name,
+            'user_id': self.user_id
         }
 
 
 class MenuItem(db.Model):
     __tablename__ = 'menu_item'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
     course = db.Column(db.String(250))
     description = db.Column(db.String(250))
     price = db.Column(db.String(8))
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
     restaurant = db.relationship(Restaurant)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
 
     @property
     def serialize(self):
@@ -47,5 +72,6 @@ class MenuItem(db.Model):
             'course': self.course,
             'description': self.description,
             'price': self.price,
-            'restaurant_id': self.restaurant_id
+            'restaurant_id': self.restaurant_id,
+            'user_id': self.user_id
         }
